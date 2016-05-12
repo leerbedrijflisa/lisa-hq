10e8fac4e780378bf3d4075b1099caae03a343ab
peterbas010 <peterbas0102@live.nl>
2016-05-12 09:57:12 +0200

    updated gulpfile.js and fixed local error's

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,14 @@
-var gulp = require('gulp');
-var browserSync = require('browser-sync');
+var gulp = require("gulp");
+var browserSync = require("browser-sync");
 
-gulp.task('serve', function() {
-  browserSync({
-    server: {
-      baseDir: 'www'
-    }
-  });
+gulp.task("serve", function() {
+    browserSync({
+        server: {
+            baseDir: "www"
+        }
+    });
+});
 
-  gulp.watch(['*.html', '*.css', '*.js'], {cwd: 'www'}, browserSync.reload);
+gulp.task("watch", [ "serve" ], function() {
+    gulp.watch(["*.js", "*.html", "*.css"], {cwd: "www"}, browserSync.reload); 
 });
